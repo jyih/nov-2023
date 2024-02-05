0726b872cab9537a50862ae0db4db48077a9cdef
jojo <johnathanyih@gmail.com>
2024-02-05 14:42:43 -0800

    update

--- demo_code/notes/W10/app.js
+++ demo_code/notes/W10/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const app  = express();
 
+app.use(express.json()) //allow us to access the body of the request
+
 app.get('/', (req, res) => {
     console.log(
         'I did the change'
     )
 })
+
+app.post('/create', (req, res) => {
+    console.log(req.body)
+})
+//if nothing in req body, you'll see undefined
 
 
 const port = 9000
