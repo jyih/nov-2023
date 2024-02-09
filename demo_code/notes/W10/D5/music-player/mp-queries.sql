d9199aa05a66ced70c1407e59ff986fc0c69c145
southwestmogrown <southwestmogrown@gmail.com>
2024-02-09 11:15:40 -0600

    aggregate functions

--- demo_code/notes/W10/D5/music-player/mp-queries.sql
+++ demo_code/notes/W10/D5/music-player/mp-queries.sql
@@ -1,2 +1,19 @@
+-- SELECT AVG(id) FROM artists;
 
+-- SELECT COUNT(*) from users;
 
+-- SELECT MIN(id) from users;
+
+-- SELECT MAX(id) from users;
+
+-- SELECT SUM(id) from albums;
+
+-- SELECT TOTAL(id) FROM playlists;
+
+SELECT artists.name, COUNT(*) AS album_count
+FROM artists
+JOIN albums
+ON artists.id = albums.artist_id
+GROUP BY artists.name
+HAVING album_count > 1
+ORDER BY album_count DESC;
