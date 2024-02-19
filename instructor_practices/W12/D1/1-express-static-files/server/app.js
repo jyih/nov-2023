51ad4696e30a2365a3d468e0fee0eb0110e34369
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-02-19 16:38:54 -0500

    End of W12D1

--- instructor_practices/W12/D1/1-express-static-files/server/app.js
+++ instructor_practices/W12/D1/1-express-static-files/server/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-// Your code here 
+// app.use('/stylesheets', express.static('assets/css'));
+// app.use(express.static('assets'));
+app.use(express.static('assets/scripts'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
