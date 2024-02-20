35480b61c185d181dc059a530838a1c8a30e75c9
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-02-20 12:01:14 -0500

    Through pagination lecture

--- W12D2_notes.js
+++ W12D2_notes.js
@@ -1,8 +1,19 @@
 /*
 
+Search Features and Pagination
 
+A couple things to think about when setting up pagination
+    Should take in page and size query params to determine pagination
+    When writing our queries, we use a couple properties for pagination
+        limit: size
+        offset: size * (page - 1)
 
+How do we handle these edge cases?
+    If page or size are undefined, they should be set to defaults of 1 and 10 respectively
 
+    If page or size are less than 1, add no pagination and return all results
+        This can be done by writing a separate query inside a conditional
+            This is highly repetitive
 
 
 
--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const { Track } = require('./db/models');
 
 const selectsRouter = require("./routes/selects");
 const insertsRouter = require("./routes/inserts");
@@ -17,6 +18,28 @@ app.use("/inserts", insertsRouter);
 app.use("/updates-and-deletes", updatesAndDeletesRouter);
 app.use("/associations", associationsRouter);
 app.use("/aggregates", aggregatesRouter);
+
+app.get('/pagination', async (req, res) => {
+  let { page, size } = req.query;
+  const pagination = {};
+
+  if (!page || isNaN(page)) page = 1;
+  if (!size || isNaN(size)) size = 10;
+
+  pagination.limit = size;
+  pagination.offset = size * (page - 1);
+
+  if (page < 1 || size < 1) {
+    delete pagination.limit;
+    delete pagination.offset;
+  };
+
+  const allTracks = await Track.findAll({
+    ...pagination,
+  });
+
+  res.json(allTracks);
+})
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`)
