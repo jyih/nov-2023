273438f07eeb65b830b8d2a8ce91315334341acc
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-02-21 11:24:03 -0500

    Through CORS lecture

---
+++ W12D3_notes.js
@@ -0,0 +1,14 @@
+/*
+
+Security
+
+CORS - Cross-Origin Resource Sharing
+    Allows other apps to send requests to our API
+    CORS policies are just middleware
+    !! Enforced by the browser !!
+
+
+
+
+
+*/
