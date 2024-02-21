b2cc937a1f9594272d213485a4ec5d822bc65d20
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-02-21 12:04:37 -0500

    Through XSS attacks lecture

--- W12D3_notes.js
+++ W12D3_notes.js
@@ -7,7 +7,10 @@ CORS - Cross-Origin Resource Sharing
     CORS policies are just middleware
     !! Enforced by the browser !!
 
-
+XSS Attacks - Cross-Site Scripting
+    Being able to inject code into someone else's site
+    Often used for malicious intent, but not always
+        Myspace is a great example
 
 
 
